87d2e2ed7485eda7236e611196ca65c998068f62
Abdul Pangandaman <abdulfahadgo566@gmail.com>
2023-01-04 13:28:37 +0800

    Update

--- array-iteration.js
+++ array-iteration.js
@@ -1,8 +1,10 @@
 const numbers = [45, 4, 9, 16, 25, 100];
-numbers.forEach(myFunction);
+numbers.forEach(function(value, index) {
+    console.log(value);
+});
 
-function myFunction(value, index, array) {
-    console.log("Value: " + value);
+// function myFunction(value, index, array) {
+    // console.log("Value: " + value);
     // console.log("Index: " + index);
     // console.log("Array: " + array);
-}
+// }
--- array-reduce.js
+++ array-reduce.js
@@ -1,20 +1,11 @@
-// const numbers = [5, 10, 15];
-// let sum = numbers.reduce(myFunction);
-
-// document.getElementById("demo").innerHTML = "The sum is " + sum;
-
-// function myFunction(total, value, index, array) {
-//     return total + value;
-// }
-
 const numbers = [5, 10, 15, 5];
-console.log(numbers.length);
+// console.log(numbers.length);
 // let sum = numbers.reduceRight(myFunction);
 let sum = numbers.reduce(myFunction);
 
 console.log(sum);
 
 function myFunction(total, value) {
-    return total + value;
     // return total;
+    return total + value;
 }
