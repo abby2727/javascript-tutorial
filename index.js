f0914c658e8dec0e5a3562c97a64bf3e5d4a4a29
Abdul Pangandaman <abdulfahadgo566@gmail.com>
2023-01-07 18:58:09 +0800

    Update

---
+++ index.js
@@ -0,0 +1,8 @@
+const cars = ["BMW", "Volvo", "Saab", "Ford"];
+let i = 0;
+
+for (; cars[i];) {
+    // text += cars[i];
+    console.log(cars[i]);
+    i++;
+}
---
+++ loop-for-of.js
@@ -0,0 +1,11 @@
+const cars = ["BMW", "Volvo", "Mini", "Lamborghini"];
+
+// for (let x of cars) {
+//     console.log(x);
+// }
+
+let language = "JavaScript";
+
+for (let x of language) {
+    console.log(x);
+}
