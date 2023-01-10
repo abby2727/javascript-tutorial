a571a6474fc05994d8d7098f9138bc18f2d8de76
Abdul Pangandaman <abdulfahadgo566@gmail.com>
2023-01-10 18:47:26 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-// let hi-me = 15;
-let hi = 15;
+try {
+    const person = '{"firstName":"John", "lastName":"Doe", "age":46}';
+    // const obj = typeof JSON.parse(person);
+    const obj = JSON.parse(person);
 
-console.log(hi);
+    console.log(obj.firstName);
+} catch (error) {
+    console.log(error);
+}
