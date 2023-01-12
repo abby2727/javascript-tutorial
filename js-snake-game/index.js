ac5d101567b1e606156eee5fe49647ff3cc93ef4
Abdul Pangandaman <abdulfahadgo566@gmail.com>
2023-01-12 18:43:55 +0800

    Display-the-canvas

---
+++ js-snake-game/index.js
@@ -0,0 +1,21 @@
+const canvas = document.getElementById('game');
+const ctx = canvas.getContext('2d');
+
+function updateGame() {
+
+}
+
+function drawGame() {
+    let speed = 7; //The interval will be seven times a second.
+
+    setTimeout(drawGame, 1000 / speed); //update screen 7 times a second
+
+    clearScreen();
+}
+
+function clearScreen() {
+    ctx.fillStyle = 'black'; // make screen black
+    ctx.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight); // black color start from 0px left, right to canvas width and canvas height
+}
+
+drawGame();
