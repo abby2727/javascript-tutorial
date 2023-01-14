ad5df71cce904d1df898a4e4a3d458b2439fed50
Abdul Pangandaman <abdulfahadgo566@gmail.com>
2023-01-14 16:35:03 +0800

    Update countSeven.js

--- countSeven.js
+++ countSeven.js
@@ -1,6 +1,6 @@
 function countSeven() {
     let count = 0;
-    for (let i = 0; i <= 30; i++) {
+    for (let i = 0; i <= 100; i++) {
         let digits = i.toString().split('');
         // count += digits.filter(d => d === '7').length;
         count = count + digits.filter(d => d === '7').length;
