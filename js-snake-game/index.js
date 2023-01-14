0eeb5859b24d11b5c7985d074a7ebbe4f4dd8b10
Abdul Pangandaman <abdulfahadgo566@gmail.com>
2023-01-14 16:32:58 +0800

    Update index.js

--- js-snake-game/index.js
+++ js-snake-game/index.js
@@ -1,12 +1,12 @@
 const canvas = document.getElementById('game');
 const ctx = canvas.getContext('2d');
-//increase snake size 
+
+// Increase snake size 
 class snakePart {
     constructor(x, y) {
         this.x = x;
         this.y = y;
     }
-
 }
 
 let speed = 7;
@@ -20,15 +20,15 @@ let headY = 10;
 const snakeParts = [];
 let tailLength = 2;
 
-//initialize the speed of snake
+// initialize the speed of snake
 let xvelocity = 0;
 let yvelocity = 0;
 
-//draw apple
+// draw apple
 let appleX = 5;
 let appleY = 5;
 
-//scores
+// initialize score
 let score = 0;
 
 // create game loop-to continously update screen
@@ -36,7 +36,8 @@ function drawGame() {
     changeSnakePosition();
     // game over logic
     let result = isGameOver();
-    if (result) {// if result is true
+    // if result is true
+    if (result) {
         return;
     }
     clearScreen();
@@ -45,30 +46,34 @@ function drawGame() {
 
     checkCollision()
     drawScore();
-    setTimeout(drawGame, 1000 / speed);//update screen 7 times a second
+    setTimeout(drawGame, 1000 / speed); //update screen 7 times a second
 }
-//Game Over function
+
+// Game Over function
 function isGameOver() {
     let gameOver = false;
-    //check whether game has started
+    // check whether game has started
     if (yvelocity === 0 && xvelocity === 0) {
         return false;
     }
-    if (headX < 0) {//if snake hits left wall
+    // if snake hits left wall
+    if (headX < 0) {
         gameOver = true;
     }
-    else if (headX === tileCount) {//if snake hits right wall
+    // if snake hits right wall
+    else if (headX === tileCount) {
         gameOver = true;
     }
-    else if (headY < 0) {//if snake hits wall at the top
+    // if snake hits top wall
+    else if (headY < 0) {
         gameOver = true;
     }
-    else if (headY === tileCount) {//if snake hits wall at the bottom
+    // if snake hits wall at the bottom
+    else if (headY === tileCount) {
         gameOver = true;
     }
 
-    //stop game when snake crush to its own body
-
+    // Stop game when snake crush to its own body
     for (let i = 0; i < snakeParts.length; i++) {
         let part = snakeParts[i];
         if (part.x === headX && part.y === headY) {//check whether any part of snake is occupying the same space
@@ -77,86 +82,82 @@ function isGameOver() {
         }
     }
 
-
-    //display text Game Over
+    // display Game Over Text
     if (gameOver) {
         ctx.fillStyle = "white";
         ctx.font = "50px verdana";
         ctx.fillText("Game Over! ", canvas.clientWidth / 6.5, canvas.clientHeight / 2);//position our text in center
     }
 
-    return gameOver;// this will stop execution of drawgame method
+    // Stop execution of drawgame method
+    return gameOver;
 }
 
-// score function
+// Implement score function
 function drawScore() {
     ctx.fillStyle = "white"// set our text color to white
     ctx.font = "10px verdena"//set font size to 10px of font family verdena
     ctx.fillText("Score: " + score, canvas.clientWidth - 50, 10);// position our score at right hand corner 
-
 }
 
 // clear our screen
 function clearScreen() {
-
-    ctx.fillStyle = 'black'// make screen black
-    ctx.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight)// black color start from 0px left, right to canvas width and canvas height
-
+    ctx.fillStyle = 'black'; // make screen black
+    ctx.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight); // black color start from 0px left, right to canvas width and canvas height
 }
+
 function drawSnake() {
-
     ctx.fillStyle = "green";
-    //loop through our snakeparts array
+    // loop through our snakeparts array
     for (let i = 0; i < snakeParts.length; i++) {
-        //draw snake parts
-        let part = snakeParts[i]
-        ctx.fillRect(part.x * tileCount, part.y * tileCount, tileSize, tileSize)
+        // draw snake parts
+        let part = snakeParts[i];
+        ctx.fillRect(part.x * tileCount, part.y * tileCount, tileSize, tileSize);
     }
-    //add parts to snake --through push
-    snakeParts.push(new snakePart(headX, headY));//put item at the end of list next to the head
+    // add parts to snake --through push
+    snakeParts.push(new snakePart(headX, headY)); // put item at the end of list next to the head
     if (snakeParts.length > tailLength) {
-        snakeParts.shift();//remove furthest item from  snake part if we have more than our tail size
-
+        snakeParts.shift(); // remove furthest item from  snake part if we have more than our tail size
     }
+
     ctx.fillStyle = "orange";
-    ctx.fillRect(headX * tileCount, headY * tileCount, tileSize, tileSize)
-
-
+    ctx.fillRect(headX * tileCount, headY * tileCount, tileSize, tileSize);
 }
+
 function changeSnakePosition() {
     headX = headX + xvelocity;
     headY = headY + yvelocity;
-
 }
+
 function drawApple() {
     ctx.fillStyle = "red";
     ctx.fillRect(appleX * tileCount, appleY * tileCount, tileSize, tileSize)
 }
+
 // check for collision and change apple position
 function checkCollision() {
     if (appleX == headX && appleY == headY) {
         appleX = Math.floor(Math.random() * tileCount);
         appleY = Math.floor(Math.random() * tileCount);
         tailLength++;
-        score++; //increase our score value
-
+        score++;
     }
 }
+
 //add event listener to our body
 document.body.addEventListener('keydown', keyDown);
 
-function keyDown()
-//up
-{
+function keyDown() {
+    // Up
     if (event.keyCode == 38) {
         //prevent snake from moving in opposite direcction
         if (yvelocity == 1)
             return;
         yvelocity = -1;
         xvelocity = 0;
-
     }
-    //down
+
+    // Down
     if (event.keyCode == 40) {
         if (yvelocity == -1)
             return;
@@ -164,14 +165,15 @@ function keyDown()
         xvelocity = 0;
     }
 
-    //left
+    // Left
     if (event.keyCode == 37) {
         if (xvelocity == 1)
             return;
         yvelocity = 0;
         xvelocity = -1;
     }
-    //right
+
+    // Right
     if (event.keyCode == 39) {
         if (xvelocity == -1)
             return;
