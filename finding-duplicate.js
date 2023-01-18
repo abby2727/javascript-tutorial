8715042e437e13a8b0694cc93bcd9bc4715ea145
Abdul Pangandaman <abdulfahadgo566@gmail.com>
2023-01-18 17:40:53 +0800

    Update

---
+++ finding-duplicate.js
@@ -0,0 +1,28 @@
+// const array = [1, 2, 2, 3, 4];
+// const duplicates = [];
+
+// array.forEach(function (value) {
+//     if (array.indexOf(value) !== array.lastIndexOf(value)) {
+//         if (!duplicates.includes(value)) {
+//             duplicates.push(value);
+//         }
+//     }
+// });
+
+// console.log(duplicates);
+
+function findDuplicates(arr) {
+    const duplicates = [];
+
+    arr.forEach(function (value) {
+        if (arr.indexOf(value) !== arr.lastIndexOf(value)) {
+            if (!duplicates.includes(value)) {
+                duplicates.push(value);
+            }
+        }
+    });
+
+    console.log(duplicates);
+}
+
+findDuplicates([1, 2, 2, 3, 4]);
--- loop-for-of.js
+++ loop-for-of.js
@@ -1,9 +1,15 @@
-const cars = ["BMW", "Volvo", "Mini", "Lamborghini"];
+// const cars = ["BMW", "Volvo", "Mini", "Lamborghini"];
 
-for (let x of cars) console.log(x);
+// for (let x of cars) console.log(x);
 
 // let language = "JavaScript";
 
 // for (let x of language) {
 //     console.log(x);
 // }
+
+const number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let num of number) {
+    console.log(num);
+}
