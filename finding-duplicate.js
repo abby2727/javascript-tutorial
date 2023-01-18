f1bde264ec11fffff53cbad69b40126141e4edc5
Abdul Pangandaman <abdulfahadgo566@gmail.com>
2023-01-18 20:30:39 +0800

    Update finding-duplicate.js

--- finding-duplicate.js
+++ finding-duplicate.js
@@ -1,28 +1,28 @@
-// const array = [1, 2, 2, 3, 4];
-// const duplicates = [];
+const array = [1, 2, 2, 3, 3, 4];
+const duplicates = [];
 
-// array.forEach(function (value) {
-//     if (array.indexOf(value) !== array.lastIndexOf(value)) {
-//         if (!duplicates.includes(value)) {
-//             duplicates.push(value);
-//         }
-//     }
-// });
-
-// console.log(duplicates);
-
-function findDuplicates(arr) {
-    const duplicates = [];
-
-    arr.forEach(function (value) {
-        if (arr.indexOf(value) !== arr.lastIndexOf(value)) {
-            if (!duplicates.includes(value)) {
-                duplicates.push(value);
-            }
+array.forEach(function (value) {
+    if (array.indexOf(value) !== array.lastIndexOf(value)) {
+        if (!duplicates.includes(value)) {
+            duplicates.push(value);
         }
-    });
+    }
+});
 
-    console.log(duplicates);
-}
+console.log(duplicates);
 
-findDuplicates([1, 2, 2, 3, 4]);
+// function findDuplicates(arr) {
+//     const duplicates = [];
+
+//     arr.forEach(function (value) {
+//         if (arr.indexOf(value) !== arr.lastIndexOf(value)) {
+//             if (!duplicates.includes(value)) {
+//                 duplicates.push(value);
+//             }
+//         }
+//     });
+
+//     console.log(duplicates);
+// }
+
+// findDuplicates([1, 2, 2, 3, 4]);
