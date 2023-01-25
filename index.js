db9aa388d993822fb452c6d76c06072a99f16471
Abdul Pangandaman <abdulfahadgo566@gmail.com>
2023-01-25 18:04:08 +0800

    Update

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ function looping() {
     for (let i = 1; i <= 5; i++) {
         for (let j = 1; j < i; j++) {
             console.log('j: ' + j);
+            // return ('j: ' + j);
         }
     }
 }
 
 looping();
+
+// function square(x) {
+//     return x * x;
+// }
+// console.log(square(3)); // 9
--- loop-for-in.js
+++ loop-for-in.js
@@ -1,5 +1,18 @@
-const numbers = [45, 4, 9, 16, 25];
+// const numbers = [45, 4, 9, 16, 25];
 
-for (let x in numbers) {
-    console.log(numbers[x]);
+// for (let x in numbers) {
+//     console.log(numbers[x]);
+// }
+
+// Working with JSON
+
+const myJSON = '{"name":"John", "age":30, "car":null}';
+const myObj = JSON.parse(myJSON);
+
+// console.log(myObj.name);
+// return false;
+
+for (const x in myObj) {
+    // console.log(x);         // name, age, car
+    console.log(myObj[x]); // John, 30, null
 }
