517731f24bc4ef4fdc10a3447c395d9b9ac7f4be
Abdul Pangandaman <abdulfahadgo566@gmail.com>
2023-01-25 16:10:08 +0800

    Update

---
+++ dom-events.js
@@ -0,0 +1,57 @@
+// * Common event listeners include:
+
+/**
+    click: fired when an element is clicked
+    mouseover: fired when the mouse pointer moves over an element
+    mouseout: fired when the mouse pointer moves out of an element
+    submit: fired when a form is submitted
+    change: fired when the value of an element changes
+    keydown: fired when a key is pressed down
+    keyup: fired when a key is released
+    load: fired when a page or an image is fully loaded
+    resize: fired when the browser window is resized
+    scroll: fired when the document view is scrolled
+*/
+
+// * Less common event listeners include:
+
+/** 
+    abort: fired when a request is aborted
+    blur: fired when an element loses focus
+    canplay: fired when a media element can start playing
+    canplaythrough: fired when a media element can be played to the end without interruption
+    contextmenu: fired when a context menu is triggered
+    cuechange: fired when a cue in a media element changes
+    dblclick: fired when an element is double-clicked
+    drag: fired when an element is dragged
+    dragend: fired when a drag operation ends
+    dragenter: fired when an element is first dragged over another element
+    dragleave: fired when an element is dragged out of another element
+    dragover: fired when an element is dragged over another element
+    dragstart: fired when a drag operation starts
+    drop: fired when an element is dropped on another element
+    durationchange: fired when the duration of a media element changes
+    emptied: fired when a media element's buffer is emptied
+    ended: fired when a media element's playback ends
+    error: fired when an error occurs
+    focus: fired when an element receives focus
+    fullscreenchange: fired when the fullscreen mode changes
+    fullscreenerror: fired when an error occurs while trying to change to fullscreen mode
+    input: fired when the value of an element changes through user input
+    invalid: fired when an element's value is invalid
+    keypress: fired when a key is pressed and released
+    loadeddata: fired when media data is loaded
+    loadedmetadata: fired when metadata of a media element is loaded
+    loadstart: fired when loading of a media element starts
+    mousedown: fired when the mouse button is pressed down on an element
+    mousemove: fired when the mouse pointer moves over an element
+    mouseup: fired when the mouse button is released on an element
+    mousewheel: fired when the mouse wheel is rotated
+    pause: fired when playback of a media element is paused
+    play: fired when playback of a media element starts or resumes
+    playing: fired when a media element starts playing
+    progress: fired when progress is made loading a media element
+    ratechange: fired when the playback rate of a media element changes
+    reset: fired when a form is reset
+    seeked: fired when a media
+*/
--- json.js
+++ json.js
@@ -3,8 +3,11 @@ let text = '{"employees":[' +
     '{"firstName":"Anna","lastName":"Smith" },' +
     '{"firstName":"Peter","lastName":"Jones" }]}';
 
+// ? Data written as an JSON array will be parsed into a JavaScript array.
+const text2 = '[ "Ford", "BMW", "Audi", "Fiat" ]';
+
 // Convert the string into a JavaScript object
-const obj = JSON.parse(text);
+const obj = JSON.parse(text2);
 
 console.log(obj);
 // console.log(obj.employees[0].firstName);
