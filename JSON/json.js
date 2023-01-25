e05f8e8882fefe4ef40918151b9b279c33a39ea2
Abdul Pangandaman <abdulfahadgo566@gmail.com>
2023-01-25 18:35:45 +0800

    Update

--- JSON/json.js
+++ JSON/json.js
@@ -7,7 +7,7 @@ let text = '{"employees":[' +
 const text2 = '[ "Ford", "BMW", "Audi", "Fiat" ]';
 
 // Convert the string into a JavaScript object
-const obj = JSON.parse(text2);
+const obj = JSON.parse(text2); 
 
-console.log(obj);
-// console.log(obj.employees[0].firstName);
+// console.log(obj);
+console.log(obj[2]);
