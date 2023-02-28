0f0c6e3df3dff0174e658a48c35daaa11a0513c0
Abdul Pangandaman <abdulfahadgo566@gmail.com>
2023-02-28 08:57:31 +0800

    Update

---
+++ async_javascript/callback.js
@@ -0,0 +1,13 @@
+// function one(call_two) {
+//     console.log('Step one');
+//     call_two();
+// }
+
+// function two() {
+//     console.log('I am-Step two');
+// }
+
+// one(two);
+// two();
+
+// * Ice Cream with Callbacks
