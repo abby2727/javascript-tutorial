b3710e6d07a900381e013e2b4ffa8839daba8c36
Abdul Pangandaman <abdulfahadgo566@gmail.com>
2023-04-12 09:10:41 +0800

    Rest Parameter

--- destructuring/spread.js
+++ destructuring/spread.js
@@ -1,4 +1,13 @@
-const { isLoggedIn, ...rest } = { isLoggedIn: false, id: 1, name: 'Ben' }
+const payload = {
+    currentPage: 1,
+    data: [1, 2, 3],
+    id: 'abc123',
+    totalPages: 5,
+    pageSize: 10
+};
 
-console.log(isLoggedIn)
+// * Gather remaining properties into new Object
+const { currentPage, data, id, ...rest } = payload;
+
+console.log(currentPage)
 console.log(rest)
