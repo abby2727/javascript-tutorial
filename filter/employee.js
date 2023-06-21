4bae6a4f5edd5ccc27b4bb9fdd667d90fe06604d
Abdul Pangandaman <abdul.pangandaman@volenday.com>
2023-06-21 23:50:07 +0800

    Update

--- filter/employee.js
+++ filter/employee.js
@@ -3,19 +3,9 @@ const employees = [
 	{ id: 10, name: 'Max', dept: 'IT' },
 	{ id: 12, name: 'Bella', dept: 'Support' },
 	{ id: 19, name: 'Amelia', dept: 'IT' },
-	{ id: 20, name: 'Robert', dept: 'IT' },
+	{ id: 20, name: 'Robert', dept: 'IT' }
 ];
 
-const onlyITEmployees = employees.filter((val) => val.dept !== 'IT');
+const onlyITEmployees = employees.filter((val) => val.dept === 'IT');
+
 console.log(onlyITEmployees);
-
-// const result = employees.map((em) => console.log((em.name)))
-// return false;
-
-// console.log(onlyITEmployees);
-
-// for (let emp in onlyITEmployees) {
-//     console.log(`${onlyITEmployees[emp].id} - ${onlyITEmployees[emp].name}`);
-// }
-
-// onlyITEmployees.map(event => console.log(event.name));
--- filter/filter.js
+++ filter/filter.js
@@ -1,8 +1,5 @@
 const num = [5, 10, 15, 20];
 
-// const over10 = num.filter((val) => {
-//     return val > 10
-// });
+const over10 = num.filter((val) => val > 10);
 
-const over10 = num.filter(val => val > 10);
 console.log(over10);
