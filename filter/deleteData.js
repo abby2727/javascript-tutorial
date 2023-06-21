5e839a697c9d9c21ccb55d86bd93e07062a0b914
Abdul Pangandaman <abdul.pangandaman@volenday.com>
2023-06-21 23:58:37 +0800

    includes in js

---
+++ filter/deleteData.js
@@ -0,0 +1,40 @@
+const data = [
+	{
+		name: 'John',
+		id: 1,
+		key: 123,
+		code: 'ABC'
+	},
+	{
+		name: 'Alice',
+		id: 2,
+		key: 456,
+		code: 'DEF'
+	},
+	{
+		name: 'Bob',
+		id: 3,
+		key: 789,
+		code: 'GHI'
+	},
+	{
+		name: 'Sarah',
+		id: 4,
+		key: 987,
+		code: 'JKL'
+	},
+	{
+		name: 'Michael',
+		id: 5,
+		key: 654,
+		code: 'MNO'
+	}
+];
+
+// const deleteID = 3;
+// const newData = data.filter((value) => value.id !== deleteID);
+// console.log(newData);
+
+const deleteIDs = [1, 2, 3];
+const newData = data.filter((value) => !deleteIDs.includes(value.id));
+console.log(newData);
