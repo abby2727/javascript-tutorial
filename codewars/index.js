58333e8328ac886f37365b649e7b42dc2de1b641
Abdul Fahad A. Pangandaman <abdul.pangandaman@volenday.com>
2023-06-25 20:56:36 +0800

    Update

--- codewars/index.js
+++ codewars/index.js
@@ -1,1 +1,17 @@
+<<<<<<< Updated upstream
 console.log('Codewars');
+=======
+// string
+// object
+
+const obj = {
+	h: 1,
+	i: 2,
+	t: 3,
+	m: 1,
+	e: 1
+};
+
+// console.log(obj.h ? 'good' : 'no good');
+console.log(obj.m);
+>>>>>>> Stashed changes
---
+++ slice.js
@@ -0,0 +1,6 @@
+const numbersX = [15, 60, 9, 12, 15, 9];
+console.log(numbersX.slice(0, 2));
+
+let numbers = [11, 22, 33, 44, 55, 66];
+let sliceArray = numbers.slice(2, 4);
+console.log(sliceArray);
