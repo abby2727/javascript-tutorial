b8bfda80c8582124d1abdccea8d3afaa15fd3fb2
Abdul Pangandaman <abdul.pangandaman@volenday.com>
2023-07-15 22:34:51 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
-const mName = 'Lenora';
-
-console.log(`Hello my name is ${mName}`);
+[
+	{
+		Id: 1,
+		File: {
+			url: 'https://...'
+		}
+	},
+	{
+		DateCreate: '2023-07-09',
+		type: 'groupByDate',
+		children: [
+			{
+				Id: 1,
+				File: {
+					url: 'https://...'
+				}
+			},
+			{
+				Id: 2,
+				File: {
+					url: 'https://...'
+				}
+			},
+			{
+				Id: 3,
+				File: {
+					url: 'https://...'
+				}
+			}
+		]
+	}
+];
