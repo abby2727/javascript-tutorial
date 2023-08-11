f7fba47ead3c0a27f1f239da0919cfc33894f4dd
Abdul Pangandaman <abdulfahadgo566@gmail.com>
2023-08-11 22:25:14 +0800

    spread vs rest operator

---
+++ rest-vs-spread-operator/rest-object.js
@@ -0,0 +1,12 @@
+const payload = {
+	currentPage: 1,
+	data: [1, 2, 3],
+	id: 'abc123',
+	totalPages: 5,
+	pageSize: 10
+};
+
+const { currentPage, data, id, newID = 'abdul', ...rest } = payload;
+
+console.log(data);
+// console.log(rest);
