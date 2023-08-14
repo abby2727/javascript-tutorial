870b2049c0e7879801fa47c34f2914e490d6ebf9
Abdul Pangandaman <abdul.pangandaman@volenday.com>
2023-08-14 17:58:50 +0800

    sorting

--- index.js
+++ index.js
@@ -1,26 +1,4 @@
-const values = [4, 7, 2, 4, 9, 4, 7, 7, 2, 4, 2, 7, 4];
+const arr2 = [1, 2, 4, 6, 13, 20];
 
-const frequencyMap = values.reduce((map, currentValue) => {
-	if (map[currentValue]) {
-		map[currentValue]++;
-	} else {
-		map[currentValue] = 1;
-	}
-	return map;
-}, {});
-
-console.log(frequencyMap);
-
-// let mostCommonElement = null;
-// let maxFrequency = 0;
-
-// for (const value in frequencyMap) {
-// 	if (frequencyMap[value] > maxFrequency) {
-// 		mostCommonElement = value;
-// 		maxFrequency = frequencyMap[value];
-// 	}
-// }
-
-// console.log(
-// 	`The most common element is ${mostCommonElement}, appearing ${maxFrequency} times.`
-// );
+const filterEven = arr2.filter((a) => a % 2 === 0);
+console.log(filterEven);
---
+++ sorting.js
@@ -0,0 +1,5 @@
+const array = [5, 10, 1, 5, 20, 15];
+
+array.sort((a, b) => a - b);
+
+console.log(array);
