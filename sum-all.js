6dd032832babd5c0c3e11f18cc9f1adef08c3b4e
Abdul Pangandaman <abdulfahadgo566@gmail.com>
2023-08-14 18:22:45 +0800

    Update

--- sum-all.js
+++ sum-all.js
@@ -1,16 +1,7 @@
-function sumAll() {
-    const array = [5, 10, 15, 30];
-    let sum = 0;
+const numbers = [2, 3, 5, 7];
+let sum = 0;
 
-    // for (let i = 0; i < array.length; i++) {
-    //     sum += array[i];
-    // }
+// numbers.forEach((n) => (sum += n));
+numbers.map((n) => (sum += n));
 
-    array.forEach(function (value) {
-        sum += value;  // value == array[i]
-    });
-    
-    console.log(sum);
-}
-
-sumAll();
+console.log(sum);
--- sum-all2.js
+++
@@ -1,9 +0,0 @@
-function sumAll() {
-    let sum = 0;
-    for (let i = 0; i < arguments.length; i++) {
-        sum += arguments[i];
-    }
-    return sum;
-}
-
-console.log(sumAll(5, 10, 15, 30));
