890276cdf51c07afde30c02b2af57a8b5e2c2e10
Abdul Pangandaman <abdulfahadgo566@gmail.com>
2023-08-14 18:37:05 +0800

    slice

--- slice/slice.js
+++ slice/slice.js
@@ -1,6 +1,6 @@
-const numbersX = [15, 60, 9, 12, 15, 9];
-console.log(numbersX.slice(0, 2));
+const array1 = [15, 60, 9, 12, 15, 9];
+console.log(array1.slice(0, -2));
 
-let numbers = [11, 22, 33, 44, 55, 66];
-let sliceArray = numbers.slice(2, 4);
+let array2 = [11, 22, 33, 44, 55, 66];
+let sliceArray = array2.slice(2, 4);
 console.log(sliceArray);
