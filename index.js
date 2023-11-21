ce0498516e65918849df8776201c68e71384c85b
Abdul Pangandaman <abdulfahadgo566@gmail.com>
2023-11-21 16:04:37 +0800

    sorting

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-let arr = ['foo', 'bar', 10, 'qux'];
+const translations = [
+	{
+		language: 'English',
+		translation: 'Hello',
+		description: 'A common greeting',
+		author: 'Unknown'
+	},
+	{
+		language: 'Spanish',
+		translation: 'Hola',
+		description: 'A common greeting in Spanish',
+		author: 'Unknown'
+	},
+	{
+		language: 'french',
+		translation: 'Bonjour',
+		description: 'A common greeting in French',
+		author: 'Unknown'
+	},
+	{
+		language: 'German',
+		translation: 'Hallo',
+		description: 'A common greeting in German',
+		author: 'Unknown'
+	}
+];
 
-const itemToReplace = 'qux';
-const indexItem = arr.indexOf(itemToReplace);
+translations.sort((a, b) => {
+	const languageA = a.language.toUpperCase(); // Convert language names to uppercase for case-insensitive comparison
+	const languageB = b.language.toUpperCase();
 
-if (indexItem > -1) {
-	arr.splice(indexItem, 1, "I'm being inserted!");
-	console.log(arr);
-} else {
-	console.log(`${itemToReplace} does not exist!`);
-}
+	if (languageA < languageB) return -1;
+	if (languageA > languageB) return 1;
+	return 0;
+});
+
+console.log(translations);
