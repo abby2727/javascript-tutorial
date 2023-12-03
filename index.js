20b6132c4ca9252ddd28cb170f551a72814fe219
Abdul Pangandaman <abdulfahadgo566@gmail.com>
2023-12-03 17:59:48 +0800

    students challenge

--- index.js
+++ index.js
@@ -1,37 +1,93 @@
-const translations = [
+const students = [
 	{
-		language: 'English',
-		translation: 'Hello',
-		description: 'A common greeting',
-		author: 'Unknown'
+		fName: 'Alex',
+		lName: 'Gonzaga',
+		age: 20,
+		hobbies: ['singing', 'dancing', 'sleeping'],
+		gender: 'female',
+		isKind: true
 	},
 	{
-		language: 'Spanish',
-		translation: 'Hola',
-		description: 'A common greeting in Spanish',
-		author: 'Unknown'
+		fName: 'Rosden',
+		lName: 'Hingpit',
+		age: 20,
+		hobbies: ['studying', 'shopping', 'reading'],
+		gender: 'female',
+		isKind: true
 	},
 	{
-		language: 'french',
-		translation: 'Bonjour',
-		description: 'A common greeting in French',
-		author: 'Unknown'
-	},
-	{
-		language: 'German',
-		translation: 'Hallo',
-		description: 'A common greeting in German',
-		author: 'Unknown'
+		fName: 'Abdul',
+		lName: 'Pangandaman',
+		age: 20,
+		hobbies: ['playing games', 'sleeping', 'chess'],
+		gender: 'male',
+		isKind: false
 	}
 ];
 
-translations.sort((a, b) => {
-	const languageA = a.language.toUpperCase(); // Convert language names to uppercase for case-insensitive comparison
-	const languageB = b.language.toUpperCase();
-
-	if (languageA < languageB) return -1;
-	if (languageA > languageB) return 1;
-	return 0;
+// * Display hobbies of each students
+/*
+students.forEach((student) => {
+	console.log(`Hobbies of ${student.fName}: `);
+	for (h of student.hobbies) {
+		console.log(h);
+	}
 });
+*/
 
-console.log(translations);
+// * Display hobbies of Abdul
+/*
+const findStudent = students.find((s) => s.fName === 'Abdul');
+console.log('Hobbies of Abdul: ');
+for (h of findStudent.hobbies) {
+	console.log(h);
+}
+*/
+
+// * Check who is adult and who is young (adult >= 21)
+// * if all are adults display "ALL are adults"
+// * if all are young display "ALL are young"
+/*
+const allAdultStudent = students.filter((s) => s.age >= 21);
+console.log('Students who are adults: ');
+if (allAdultStudent.length === 0) {
+	console.log('No adult students.');
+} else {
+	allAdultStudent.forEach((s) => {
+		console.log(s.fName);
+	});
+}
+
+const allYoungStudent = students.filter((s) => s.age < 21);
+console.log('Students who are young: ');
+if (allYoungStudent.length === 0) {
+	console.log('No young students.');
+} else {
+	allYoungStudent.forEach((s) => {
+		console.log(s.fName);
+	});
+}
+
+console.log('');
+
+const allAreAdults = students.every((s) => s.age >= 21);
+if (allAreAdults) {
+	console.log('ALL are adults.');
+}
+
+const allAreYoung = students.every((s) => s.age < 21);
+if (allAreYoung) {
+	console.log('ALL are young.');
+}
+*/
+
+// * Modify age of Abdul
+const findAbdul = students.find((s) => s.fName === 'Abdul');
+findAbdul.age = 24;
+console.log(findAbdul);
+
+// * Modify age of each students
+students.forEach((s) => {
+	s.age = 30;
+});
+console.log(students);
